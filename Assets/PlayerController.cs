f605646842128039c9ebf4bc6c3a0b7e2c0174b4
jacobmm <tablej@hotmail.com>
2019-05-15 23:15:52 -0700

    added the project to git

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    protected override void ComputeVelocity()
+    {
+        base.ComputeVelocity();
+
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+}
